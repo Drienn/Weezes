9b5be3ddf3ff80bbd8ff9b6f00adde5c27449ce8
Emit Dutcher <emit.dutcher@wearedauntless.com>
2018-12-19 14:13:23 -0700

    all prepared

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure, addDecorator } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
+
+addDecorator(withNotes);
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../src', true, /.stories.(js|jsx)$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
